481c24b6a8fb8f7c846f85010c53f757d39870c5
eltonjncorreia <eltonjncorreia@gmail.com>
2021-06-09 19:44:04 -0300

    refactor: change function Repeat

--- 03_iterator.go
+++ 03_iterator.go
@@ -1,9 +1,11 @@
 package main
 
+const countRepeating = 5
+
 func Repeat(name string) string {
 	var r string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < countRepeating; i++ {
 		r += name
 	}
 	return r
