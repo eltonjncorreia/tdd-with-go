e493214f04cb74689a093339f06be76fc42c73d8
eltonjncorreia <eltonjncorreia@gmail.com>
2021-06-08 22:31:04 -0300

    refactor: add subtest with Helper()

--- 01_hello_world_test.go
+++ 01_hello_world_test.go
@@ -3,22 +3,24 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	t.Run("Walk Hello for people", func(t *testing.T) {
-		params := "Elton"
-		result := HelloWorld(params)
-		expected := "Hello, Elton"
+	checkMessage := func(t *testing.T, result, expected, params string) {
+		t.Helper()
 
 		if result != expected {
 			t.Errorf("result '%s', expected '%s', params: %s", result, expected, params)
 		}
+	}
+
+	t.Run("Walk Hello for people", func(t *testing.T) {
+		params := "Elton"
+		result := HelloWorld(params)
+		expected := "Hello, Elton"
+		checkMessage(t, result, expected, params)
 	})
 
 	t.Run("says 'Hello world' when an empty string", func(t *testing.T) {
 		result := HelloWorld("")
 		expected := "Hello, world"
-
-		if result != expected {
-			t.Errorf("result '%s', expected '%s'", result, expected)
-		}
+		checkMessage(t, result, expected, "")
 	})
 }
